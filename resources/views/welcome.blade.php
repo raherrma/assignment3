e17d2bf947b8de9b31d4be89e9e417abc2951d7a
Pål Syvertsen Stakvik <palsyvertsenstakvik@public-142-163.hig.no>
2018-06-04 16:48:43 +0200

    change header text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dream Team
+                    Dream Team Woho !!
                 </div>
 
                 <div class="links">
