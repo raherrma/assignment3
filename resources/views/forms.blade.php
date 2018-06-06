8e18c34454b9fb78c359f74034878519736ff3be
Pål Syvertsen Stakvik <palsyvertsenstakvik@ka-127.studby.hig.no>
2018-06-06 10:56:56 +0200

    Add registration forms style

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -1,23 +1,25 @@
 @extends ('layouts.master')
 
 @section ('content')
-    
-<form action="/my-handling-form-page" method="post">
-    <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="user_name">
-    </div>
-    <div>
-        <label for="mail">E-mail:</label>
-        <input type="email" id="mail" name="user_mail">
-    </div>
-    <div>
-        <label for="password">Password:</label>
-        <input type="password" id="pwd" name="pwd">
-    </div>
-    <div class="button">
-        <button type="submit">Register user</button>
-    </div>
-</form>
+
+<div class="row">
+    <form class="p-5" action="/my-handling-form-page" method="post">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="user_name">
+        </div>
+        <div>
+            <label for="mail">E-mail:</label>
+            <input type="email" id="mail" name="user_mail">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="pwd">
+        </div>
+        <div class="button">
+            <button type="submit">Register user</button>
+        </div>
+    </form>
+</div>
 
 @endsection
